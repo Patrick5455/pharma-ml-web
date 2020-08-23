fc1a71b575e604f3421758c52b159ccced5d9160
Patrick5455 <patrickojunde@gmail.com>
2020-08-23 03:35:12 +0100

    Added Web Files

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
 from flask import Flask
+import joblib
+import numpy as np
+import pandas as pd
+import csv
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def homepage():
     return 'Hello World!'
+
+
+@app.route("/upload")
+def upload_data():
+    return "upload"
+
+
+@app.route("/save")
+def save_date():
+    return "save"
+
+
+@app.route("/predict", methods=['POST'])
+def model_predict():
+    return "predicting"
+
+
+@app.route("/result")
+def model_result():
+    return "result"
 
 
 if __name__ == '__main__':
